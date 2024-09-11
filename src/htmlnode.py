7eb396cd9f7d5c9c16b1b1ff9ed1f5010a19bd81
Jacob234 <Jacobbenkell@gmail.com>
2024-09-11 14:55:00 -0400

    C: Created htmlnode

---
+++ src/htmlnode.py
@@ -0,0 +1,19 @@
+class HTMLNode():
+
+    def __init__(self, tag = None, value = None, children = None, props = None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        html = ""
+        for key in self.props:
+            html = html + " " + key + "=" + self.props[key]
+        return html
+    
+    def __repr__(self):
+        return repr("tag: " + self.tag "\nvalue: " + self.value + "\nchildren: " + self.children + "\nprops:" + self.props)
