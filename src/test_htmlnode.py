82647a41ec4cf656815dd330d8fcc3d221e5f43f
Jacob234 <Jacobbenkell@gmail.com>
2024-09-11 21:00:04 -0400

    E: Unit Test Implementation (help of Claude)

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -2,23 +2,51 @@ import unittest
 
 from htmlnode import HTMLNode
 
+"""
+HTMLNode (tag, value, children, prompts):
 
-class TestTextNode(unittest.TestCase):
-    def test_empty_eq(self):
-        node = HTMLNode()
-        node2 = HTMLNode(None, None, None, None)
-        self.assertEqual(node, node2)
+tag (string): represents the HTML tag ("p", "a", "h1")
+value (string): represents value
+children (list of HTMLNode objects): representing children
+prompts (dictionary of k-v pairs): represents attributes of HTML tag
 
-    def test_dif(self):
-        node = HTMLNode("text", "bold")
-        node2 = HTMLNode("text", "underline")
-        self.assertNotEqual(node, node2)
 
-    def test_url(self):
-        node = HTMLNode("text", "bold", "None")
-        node2 = HTMLNode("text", "underline", None)
-        self.assertNotEqual(node, node2)
+An HTMLNode without a tag will just render as raw text
+An HTMLNode without a value will be assumed to have children
+An HTMLNode without children will be assumed to have a value
+An HTMLNode without props simply won't have any attributes
+"""
 
+
+class TestHTMLNode(unittest.TestCase):
+
+    def setUp(self):
+        self.empty_node = HTMLNode()
+        self.para_node = HTMLNode("p", "This is a paragraph")
+        self.link_node = HTMLNode("a", "Click me", props={"href":"hi.com"})
+        self.parent_node = HTMLNode("p", children=[self.para_node, self.link_node])
+
+
+    
+    
+    def test_constructor(self):
+        self.assertIsNone(self.empty_node.tag)
+        self.assertEqual(self.para_node.tag, "p")
+        self.assertEqual(self.link_node.value, "Click me")
+
+    def test_props_to_html(self):
+        self.assertEqual(self.empty_node.props_to_html(), "")
+        self.assertEqual(self.para_node.props_to_html(), "")
+        self.assertEqual(self.link_node.props_to_html(), ' href="hi.com"')
+
+    def test_props_to_html_multiple_props(self):
+        node = HTMLNode(props={"class": "btn", "id": "submit"})
+        self.assertIn(' class="btn"', node.props_to_html())
+        self.assertIn(' id="submit"', node.props_to_html())
+
+    def test_repr(self):
+        expected = "HTMLNode(tag='p', value='This is a paragraph', children=None, props=None)"
+        self.assertEqual(repr(self.para_node), expected)
 
 if __name__ == "__main__":
     unittest.main()
